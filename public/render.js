2e19aed73aa7b572652ebd7c3b7ccbdf7038e881
timcoe429 <timcoe429@gmail.com>
2025-06-25 19:52:25 -0400

    Update render.js

--- public/render.js
+++ public/render.js
@@ -374,7 +374,7 @@ class Renderer {
             container.innerHTML = '';
             return;
         }
-        
+    }      
         container.innerHTML = `
             <div class="bg-gradient-to-r from-purple-50 to-pink-50 rounded-xl p-6 mb-6 shadow-sm border border-purple-200">
                 <div class="flex items-center justify-between mb-4">
